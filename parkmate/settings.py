a6604004a746fd6f137206b8b9c3a3f8e173abc6
thanushsutharsan <thanush23@yahoo.com>
2026-09-12 00:09:54 +0100

    fix: repair Wikimedia parking image loading

--- parkmate/settings.py
+++ parkmate/settings.py
@@ -139,7 +139,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
